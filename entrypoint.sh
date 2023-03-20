5bba88e75d6a8a592b5f3bf1462a9e412b0dff25
Progi1984 <progi1984@gmail.com>
2023-03-20 07:24:01 +0000

    Initial Commit

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+exec docker run \
+    -v "/var/run/docker.sock":"/var/run/docker.sock" \
+    -p 8080:8080 \
+    -e KEYCLOAK_ADMIN='admin' \
+    -e KEYCLOAK_ADMIN_PASSWORD='admin' \
+    quay.io/keycloak/keycloak:19.0.3 \
+    start-dev
