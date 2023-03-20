45d54a14e02adb54fc32232a0272577ad8f3bf88
Progi1984 <franck.lefevre@prestashop.com>
2023-03-20 13:47:55 +0100

    Setup Keycloak : Define HTTPS Port

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ exec docker run \
     -d \
     -v "/var/run/docker.sock":"/var/run/docker.sock" \
     -p $INPUT_KEYCLOAK_HTTP_PORT:8080 \
+    -p $INPUT_KEYCLOAK_HTTPS_PORT:8443 \
     -e KEYCLOAK_ADMIN=$INPUT_KEYCLOAK_ADMIN_USER \
     -e KEYCLOAK_ADMIN_PASSWORD=$INPUT_KEYCLOAK_ADMIN_PASS \
     quay.io/keycloak/keycloak:$INPUT_KEYCLOAK_VERSION \
